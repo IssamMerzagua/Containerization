7ff31508b0e062291d57965721eb67b6b3fef249
IssamMerzagua <merzagua.issam@gmail.com>
2022-12-18 00:43:27 +0100

    commit du projet Containerization

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <EncryptedMessage msg="Welcome to my app"/>
+</template>
+
+<script>
+import EncryptedMessage from './components/EncryptedMessage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EncryptedMessage
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EncryptedMessage.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+  </div>
+
+  <div id="app">
+  <h4>Fonction SHA256</h4>
+  <form action="#" method="post">
+    Entez un texte :<br/>
+    <input type="text" size="25" v-model="message" /><br/><br/>
+    Chaîne de hachage (SHA256) :
+    <button type="button" v-on:click="sha256Function()" > Go </button> <br/><br/>
+    {{encryptedMessage}}
+    <br/>
+  </form>
+  </div>
+
+</template>
+<script>
+import {sha256} from "js-sha256";
+export default {
+  name: 'EncryptedMessage',
+  props: {
+    msg: String
+  },
+  data()
+  {
+    return {
+        message:null,
+        encryptedMessage:null
+    }
+  },
+  methods: {
+    sha256Function() {
+      this.encryptedMessage= sha256(this.message);
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
